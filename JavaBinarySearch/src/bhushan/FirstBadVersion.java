f2312205819c6ef7e71a00a33ddd741aa9b73e4f
bhushandeore1 <pbhushandeore@gmail.com>
2023-05-28 02:01:42 +0530

    First bad version example leetcode

---
+++ JavaBinarySearch/src/bhushan/FirstBadVersion.java
@@ -0,0 +1,30 @@
+package bhushan;
+
+
+//Leetcode 278. First Bad Version
+
+public class FirstBadVersion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public class Solution extends VersionControl {
+	    public int firstBadVersion(int n) {
+	        int start=0;
+	        int end=n-1;
+	        while(start<=end){
+	            int mid=start+(end-start)/2;
+	            if(isBadVersion(mid)){
+	                end=mid;
+	            }
+	            else{
+	                start=mid+1;
+	            }
+	        }
+	        return start;
+	    }
+	}
+
+
+}
